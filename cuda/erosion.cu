6fe804206ec761b9924c1ffcbdbc4a2891d6c763
Hadrien Delassus <hadrien.delassus@epita.fr>
2019-05-15 17:51:21 +0200

    [GPU] added comments

--- cuda/erosion.cu
+++ cuda/erosion.cu
@@ -4,20 +4,15 @@
 #include <iostream>
 #include <stdio.h>
 
-__global__ void helloOnGPU()
-{
-    printf("Hello World on GPU!\n");
-}
-
 __global__ void erosion(uint8_t *orig, uint8_t *morphed, unsigned width) {
     int indexX = blockIdx.x * blockDim.x + threadIdx.x;
-    // int indexY = blockIdx.y * blockDim.y + threadIdx.y;
+    int indexY = blockIdx.y * blockDim.y + threadIdx.y;
     //printf("%d, %c ", indexX, orig[indexX]);
-    //printf("%d ", indexX);
+    printf("[x: %d, y: %d] ", indexX, indexY);
     // printf("%c ", orig[indexX]);
     // morphed[indexY * width + indexX] = orig[indexY * width + indexX];
 
-    morphed[indexX] = orig[indexX];
+    morphed[indexY * width + indexX] = orig[indexY * width + indexX];
 }
 
 void printMat(uint8_t *mat, unsigned size) {
@@ -32,16 +27,17 @@ void printMat(uint8_t *mat, unsigned size) {
 int main(int argc, char **argv)
 {
 
-    // helloOnGPU<<<1, 10>>>();
-
+    // data on HOST memory
     uint8_t *ma = new uint8_t[9] { 'E', 'A', 'j', '1', 'h', '0', 'q', 'd', 'w' };
     uint8_t *mo = new uint8_t[9] { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' };
 
+    // GPU memory allocation
     uint8_t *mat;
     uint8_t *morphed;
     cudaMalloc(&mat, 9 * sizeof (uint8_t));
     cudaMalloc(&morphed, 9 * sizeof (uint8_t));
 
+    // copy HOST memory to GPU memory
     cudaMemcpy(mat, ma, sizeof(uint8_t) * 9, cudaMemcpyHostToDevice);
     cudaMemcpy(morphed, mo, sizeof(uint8_t) * 9, cudaMemcpyHostToDevice);
 
@@ -52,12 +48,13 @@ int main(int argc, char **argv)
     const int n = 128 * 1024;
     int blockSize = 512;
     int nbBlocks = n / nthreads;
-    // dim3 grids(3, 3);
-    // dim3 threads
-    // std::cout << "nbBlocks: " << nbBlocks << "  blockSize: " << blockSize << std::endl;
-    
-    erosion<<<1, 9>>>(mat, morphed, 3);
+    dim3 grids(3, 3);
+    dim3 threads(3, 3);
 
+    // compute on GPU
+    erosion<<<grids, 1>>>(mat, morphed, 3);
+
+    // copy back GPU memory to HOST memory
     cudaMemcpy(mo, morphed, sizeof (uint8_t) * 9, cudaMemcpyDeviceToHost);
 
     printMat(mo, 9);
